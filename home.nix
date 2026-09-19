952192d36ec86b2578ca10bf85aa9ae7ef98709a
Jesse Jin <jesse@jin.com>
2026-09-18 22:44:50 -0700

    Manage git identity declaratively via programs.git

--- home.nix
+++ home.nix
@@ -66,6 +66,14 @@ in
     };
   };
 
+  programs.git = {
+    enable = true;
+    settings.user = {
+      name = "Jesse Jin";
+      email = "jesse@jin.com";
+    };
+  };
+
   # Edit-in-place: the real file stays in my repo, ~/.config just points at it.
   home.file.".config/wezterm".source =
     config.lib.file.mkOutOfStoreSymlink "${dotfiles}/home/.config/wezterm";
